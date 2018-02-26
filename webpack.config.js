23f63f08a38b5825f3c515228b8d4ad59395cf25
Matthew Szekalski <mattszekalski@icloud.com>
2018-02-26 16:36:49 -0500

    moved webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './main.js',
+  entry: './snake/js/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
